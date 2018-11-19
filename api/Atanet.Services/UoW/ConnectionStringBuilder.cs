cba3268bafc81be6e98fdfaae4584cff71f545e1
Thomas Gassmann <thomas.gassmann@hotmail.com>
2018-11-19 23:56:56 +0100

    fixed backend

--- api/Atanet.Services/UoW/ConnectionStringBuilder.cs
+++ api/Atanet.Services/UoW/ConnectionStringBuilder.cs
@@ -1,6 +1,9 @@
 namespace Atanet.Services.UoW
 {
     using System;
+    using System.Linq;
+    using System.Net;
+    using System.Net.NetworkInformation;
 
     public class ConnectionStringBuilder : IConnectionStringBuilder
     {
@@ -8,11 +11,26 @@ namespace Atanet.Services.UoW
 
         public string ConstructConnectionStringFromEnvironment()
         {
+            var gateway = GetDefaultGateway();
             var port = Environment.GetEnvironmentVariable("DATABASE_PORT");
             var user = Environment.GetEnvironmentVariable("MYSQL_USER");
             var password = Environment.GetEnvironmentVariable("MYSQL_PASSWORD");
             var databaseName = Environment.GetEnvironmentVariable("MYSQL_DATABASE");
-            return $"Server=${HostName};Port=${port.ToString()};Database=${databaseName};Uid=${user};Pwd=${password};";
+            var connectionString = $"server={gateway.ToString()};port={port};database={databaseName};userid=root;password={password};";
+            Console.WriteLine(connectionString);
+            return connectionString;
+        }
+
+        public static IPAddress GetDefaultGateway()
+        {
+            return NetworkInterface
+                .GetAllNetworkInterfaces()
+                .Where(n => n.OperationalStatus == OperationalStatus.Up)
+                .Where(n => n.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                .SelectMany(n => n.GetIPProperties()?.GatewayAddresses)
+                .Select(g => g?.Address)
+                .Where(a => a != null)
+                .FirstOrDefault();
         }
     }
 }
