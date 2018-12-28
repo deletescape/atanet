e7dd90b8cae640066e6a0e41b81df8b45a5a9743
Thomas Gassmann <thomas.gassmann@hotmail.com>
2018-12-28 23:33:01 +0100

    support utf8

--- api/Atanet.Model/Data/Comment.cs
+++ api/Atanet.Model/Data/Comment.cs
@@ -2,11 +2,13 @@
 {
     using Atanet.Model.Interfaces;
     using System;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public class Comment : IIdentifiable, ICreated, IUserCreatedEntity
     {
         public long Id { get; set; }
 
+        [Column(TypeName = "varchar(10000) character set utf8mb4")]
         public string Text { get; set; }
 
         public long PostId { get; set; }
--- api/Atanet.Model/Data/Post.cs
+++ api/Atanet.Model/Data/Post.cs
@@ -2,11 +2,13 @@
 {
     using Atanet.Model.Interfaces;
     using System;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public class Post : IIdentifiable, ICreated, IUserCreatedEntity
     {
         public long Id { get; set; }
 
+        [Column(TypeName = "varchar(10000) character set utf8mb4")]
         public string Text { get; set; }
 
         public long PictureId { get; set; }
